24f319069815300f5c1bfa245dc7d8525a3a039a
remyauda <remyauda@yahoo.fr>
2017-03-05 22:31:06 +0100

    [ADD/UPDATE] This project shows now how to use the logger into the console, a file, a rolling file, and into a database.

--- src/testLog4j1/TestLog4j1.java
+++ src/testLog4j1/TestLog4j1.java
@@ -6,7 +6,9 @@ import org.apache.logging.log4j.LogManager;
 public class TestLog4j1 {
 
 	private static final Logger logger = LogManager.getLogger(TestLog4j1.class.getName());
+	
 	public static void main(String[] args) {
+		
 		if(logger.isDebugEnabled()){
 			System.out.println("debug test"); // Will not be display if the logger isn't in debug priority because of the if statement.
 			logger.debug("msg de debogage");
@@ -23,5 +25,6 @@ public class TestLog4j1 {
 		if(logger.isFatalEnabled()){
 			logger.fatal("msg d'erreur fatale");
 		}
+		
 	}
 }
